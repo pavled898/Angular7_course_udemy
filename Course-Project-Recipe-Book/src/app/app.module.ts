e3ece07b33714d82e57986c801f14e61d165756e
Pavle <pavled989@gmail.com>
2019-05-27 17:35:01 +0200

    seccond commit

--- Course-Project-Recipe-Book/src/app/app.module.ts
+++ Course-Project-Recipe-Book/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { RecipeDetailsComponent } from './recipes/recipe-details/recipe-details.component';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,7 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
     RecipesComponent,
     RecipeListComponent,
     RecipeItemComponent,
+    RecipeDetailsComponent,
     ShoppingListComponent,
     ShoppingEditComponent
   ],
